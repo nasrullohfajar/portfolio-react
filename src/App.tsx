f0a68bf73ce0e8bdd5b5c37322cddf3d08cbe4cc
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-24 17:27:36 +0700

    add: background component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import StarsBackground from "./component/background/StarsBackground";
+import ShootingStars from "./component/background/ShootingStars";
 import Hero from "./component/Hero";
 
 function App() {
   return (
     <>
+      <StarsBackground starDensity={0.00005} />
+
+      <ShootingStars />
       <Hero />
     </>
   );
